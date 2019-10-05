241f6992ef822256e9beed4b9b4c7f1ed25160c4
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-10-05 22:14:43 +0300

    Spring AOP Demo project by FlameXander

---
+++ spring-aop/src/main/java/com/geekbrains/aop/ExternalSystemUserDAO.java
@@ -0,0 +1,11 @@
+package com.geekbrains.aop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExternalSystemUserDAO {
+
+    public void addUser() {
+        System.out.println("New External User added");
+    }
+}
